c3a9adc16ed03c9a8a7d478642ea55be891b520d
Ivaylo Dimitrov <freemangordon@abv.bg>
2012-08-26 21:53:52 +0100

    Fix licensing
    Signed-off-by: Ivaylo Dimitrov <freemangordon@abv.bg>

--- hildon-im-fkb/hildon-im-western-fkb.c
+++ hildon-im-fkb/hildon-im-western-fkb.c
@@ -5,11 +5,11 @@
 
    This file is part of hildon-input-method-plugins.
 
-   Libbme is free software; you can redistribute it and/or modify
+   hildon-input-method-plugins is free software; you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License
    version 2.1 as published by the Free Software Foundation.
 
-   Libbme is distributed in the hope that it will be useful,
+   hildon-input-method-plugins is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
--- hildon-im-western-plugin-common3/hildon-im-western-plugin-common.c
+++ hildon-im-western-plugin-common3/hildon-im-western-plugin-common.c
@@ -1,3 +1,23 @@
+/**
+   @file hildon-im-western-plugin-common.c
+
+   Copyright (C) 2012 Ivaylo Dimitrov <freemangordon@abv.bg>
+
+   This file is part of hildon-input-method-plugins.
+
+   hildon-input-method-plugins is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License
+   version 2.1 as published by the Free Software Foundation.
+
+   hildon-input-method-plugins is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with hildon-input-method-plugins. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <glib.h>
 #include <errno.h>
 #include <string.h>
