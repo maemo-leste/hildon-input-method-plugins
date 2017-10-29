133b3fe1932f7a2b467983c8457d24f5da272b76
Jonathan Wilson <jfwfreo@tpgi.com.au>
2017-10-29 04:38:48 -0400

    Move hildon-im-word-completer to shared so hildon_keyboard_assistant.so can use it

---
+++ shared/hildon-im-xkb.c
@@ -0,0 +1,40 @@
+#include <glib.h>
+#include "hildon-im-xkb.h"
+void hildon_im_xkb_set_sticky(int sticky, const gchar *name)
+{
+  assert(0);
+  //todo
+}
+gchar *hildon_im_xkb_get_name(void)
+{
+  assert(0);
+  return NULL;
+  //todo
+}
+GSList *hildon_im_xkb_get_keyboard_names()
+{
+  assert(0);
+  return NULL;
+  //todo
+}
+void hildon_im_xkb_print_devices()
+{
+  assert(0);
+  //todo
+}
+void hildon_im_xkb_set_rate(gint delay, gint interval, const gchar *name)
+{
+  assert(0);
+  //todo
+}
+void hildon_im_xkb_set_map(gchar *model, gchar *layout, const gchar *name)
+{
+  assert(0);
+  //todo
+}
+const gchar *hildon_im_xkb_get_map()
+{
+  assert(0);
+  return NULL;
+  //todo
+}
