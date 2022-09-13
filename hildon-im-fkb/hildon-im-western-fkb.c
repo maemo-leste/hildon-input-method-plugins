d47b0b194c0ee4437b7f8a9d66c821aa54dba24d
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2022-09-13 08:57:57 +0300

    fkb: Fix keyboard size when launched in portrait
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- hildon-im-fkb/hildon-im-western-fkb.c
+++ hildon-im-fkb/hildon-im-western-fkb.c
@@ -270,9 +270,11 @@ hildon_im_western_fkb_init(HildonIMWesternFKB *fkb)
   GtkWidget *vkb;
   PangoFontDescription *font;
   GtkRequisition dimension;
+  GdkScreen *screen;
 
-  dimension.width =
-      gdk_screen_get_width(gtk_widget_get_screen(GTK_WIDGET(fkb)));
+  screen = gtk_widget_get_screen(GTK_WIDGET(fkb));
+  dimension.width = MAX(gdk_screen_get_width(screen),
+                        gdk_screen_get_height(screen));
   dimension.height = HILDON_IM_WESTERN_FKB_HEIGHT;
 
   priv = HILDON_IM_WESTERN_FKB_GET_PRIVATE (HILDON_IM_WESTERN_FKB(fkb));
@@ -1008,7 +1010,7 @@ fkb_window_create(HildonIMWesternFKB *fkb)
         gtk_image_new_from_icon_name("keyboard_enter", (GtkIconSize)-1));
   priv->enter_button = button;
 
-  gtk_widget_set_size_request(priv->vkb, -1, 210);
+  gtk_widget_set_size_request(priv->vkb, -1, HILDON_IM_WESTERN_FKB_HEIGHT);
   hbox1 = gtk_hbox_new(FALSE, 0);
 
   gtk_box_pack_start(GTK_BOX(hbox1), priv->menu_button, FALSE, FALSE, 0);
